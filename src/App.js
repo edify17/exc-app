4a3cb1930a49a0d0466723bab5f7f0bc1193e291
Sakshi Gupta <sakshgup@adobe.com>
2019-04-26 14:17:10 +0530

    integrating pulse in exc-app alike repo

--- src/App.js
+++ src/App.js
@@ -17,11 +17,25 @@ export default class App extends Component {
               <p className="App-name">Adobe Excperience Cloud</p>
               <Link to="/module1" className="App-route">Module 1</Link>
               <Link to="/module2" className="App-route">Module 2</Link>
+              <Link to="/pulse1" className="App-route">Pulse 2</Link>
           </header>
           <Route path="/module1" component={ModuleA} />
           <Route path="/module2" component={ModuleB} />
+          <Route path="/pulse1" component={Pulse1} />
         </Router>
       </div>
     );
   }
+}
+
+function Pulse1(props) {
+  // let s = document.createElement('script');
+  //   s.src = 'http://localhost:3535/bundle.js';
+  //   s.setAttribute('type', 'text/javascript');
+  //   document.body.appendChild(s);
+  // console.log(Pulse);
+  return (
+    <Pulse.App />
+    )
+    // <script type="text/javascript" src="http://localhost:3535/bundle.js"></script>
 }
